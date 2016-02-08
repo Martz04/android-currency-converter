eed3437baca3f452f48af07aef4fb6264f4e732a
Mario G <mariogl00@gmail.com>
2016-02-08 09:48:03 -0600

    Adding currencyPreferences missing

---
+++ app/src/main/java/com/example/mariogl/exam1/CurrencyPreferences.java
@@ -0,0 +1,23 @@
+package com.example.mariogl.exam1;
+
+/**
+ * Created by mariogl on 08/02/16.
+ */
+public class CurrencyPreferences {
+
+    public static final String CURRENCY_PREFS_NAME = "MyCurrencyPrefs";
+
+    public static final String CURRENCY_FROM_KEY = "currency_from_key";
+    public static final String CURRENCY_TO_KEY = "currency_to_key";
+
+    public static final String CURRENCY_FROM_DEFAULT = "USD";
+    public static final String CURRENCY_TO_DEFAULT = "MXN";
+
+    public static final String CURRENCY_USD = "USD";
+    public static final String CURRENCY_EUR = "EUR";
+    public static final String CURRENCY_MXN = "MXN";
+
+    public static final Double AMMOUNT_USD = 1.0;
+    public static final Double AMMOUNT_EUR = 0.90;
+    public static final Double AMMOUNT_MXN = 18.66;
+}
